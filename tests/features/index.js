55015e974d9ba762b1ddf73cf78f6f920c84ba70
kirya <cloudkserg11@gmail.com>
2018-10-01 01:23:50 +0300

    fixed

--- tests/features/index.js
+++ tests/features/index.js
@@ -63,12 +63,11 @@ module.exports = (ctx) => {
            ctx.amqp.channel.consume(`app_${config.rabbit.serviceName}_test_features.balance`, 
              async data => {
 
-               if (!data)
+               if (!data || !tx)
                  return;
                const message = JSON.parse(data.content.toString());
 
-               console.log(tx);
-               expect(tx.timestamp).to.equal(message.tx.timestamp);
+               expect(tx.id).to.equal(message.tx.id);
                expect(message.balance).to.eq(balance1.toString());
                expect(message.address).to.eq(ctx.accounts[1]);
                await ctx.amqp.channel.deleteQueue(`app_${config.rabbit.serviceName}_test_features.balance`);
@@ -86,7 +85,7 @@ module.exports = (ctx) => {
              `app_${config.rabbit.serviceName}_test_features2.balance`,
              async data => {
 
-               if (!data)
+               if (!data || !tx)
                  return;
 
                const message = JSON.parse(data.content.toString());
